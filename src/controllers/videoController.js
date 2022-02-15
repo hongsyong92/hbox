3fe5ed2b256e36d88e786516f2a132d99f8f590b
hongsyong92 <ghdgus24@naver.com>
2022-02-15 17:33:04 +0900

    pug 끝

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,32 @@
-export const trending = (req, res) => res.render("home");
+export const trending = (req, res) => {
+  const videos = [
+    {
+      title: "First Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2분 전",
+      views: 55,
+      id: 1,
+    },
+    {
+      title: "Second Video",
+      rating: 4,
+      comments: 1,
+      createdAt: "22분 전",
+      views: 55,
+      id: 1,
+    },
+    {
+      title: "Third Video",
+      rating: 3,
+      comments: 0,
+      createdAt: "12분 전",
+      views: 55,
+      id: 1,
+    },
+  ];
+  return res.render("home", { pageTitle: "Home", videos });
+};
 export const see = (req, res) => res.render("watch");
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search Videos");
