908abcf7e3287fd15dbf65c59666ba625e2cdf3b
hongsyong92 <ghdgus24@naver.com>
2022-02-18 17:38:32 +0900

    db 적용중

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,53 +1,25 @@
-const videos = [
-  {
-    title: "First Video",
-    rating: 5,
-    comments: 2,
-    createdAt: "2분 전",
-    views: 55,
-    id: 1,
-  },
-  {
-    title: "Second Video",
-    rating: 4,
-    comments: 1,
-    createdAt: "22분 전",
-    views: 55,
-    id: 2,
-  },
-  {
-    title: "Third Video",
-    rating: 3,
-    comments: 0,
-    createdAt: "12분 전",
-    views: 55,
-    id: 3,
-  },
-];
+import Video from "../models/Video";
 
-export const trending = (req, res) => {
-  return res.render("home", { pageTitle: "Home", videos });
+export const home = (req, res) => {
+  Video.find({}, (error, videos) => {
+    return res.render("home", { pageTitle: "Home", videos });
+  });
 };
 export const watch = (req, res) => {
   const { id } = req.params;
-  const video = videos[id - 1];
   return res.render("watch", {
-    pageTitle: `${video.title} 영상 시청 중`,
-    video,
+    pageTitle: `영상 시청 중`,
   });
 };
 export const getEdit = (req, res) => {
   const { id } = req.params;
-  const video = videos[id - 1];
   return res.render("edit", {
-    pageTitle: `${video.title} 영상을 수정 중`,
-    video,
+    pageTitle: `영상 수정 중`,
   });
 };
 export const postEdit = (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
-  videos[id - 1].title = title;
   return res.redirect(`/videos/${id}`);
 };
 
@@ -56,15 +28,6 @@ export const getUpload = (req, res) => {
 };
 export const postUpload = (req, res) => {
   const { title } = req.body;
-  const newVideo = {
-    title,
-    rating: 0,
-    comments: 0,
-    createdAt: "지금",
-    views: 0,
-    id: videos.length + 1,
-  };
-  videos.push(newVideo);
   return res.redirect(`/`);
 };
 
