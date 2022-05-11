03ca56ba988e1959957e0e45e1c1829de21e23de
SYONG <ghdgus24@naver.com>
2022-05-11 11:09:25 +0900

    로그인까지

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,8 +1,10 @@
 import User from "../models/User";
+import bcrypt from "bcrypt";
 
 export const getJoin = (req, res) =>
   res.render("join", { pageTitle: "가입하기" });
 
+// 회원가입 로직
 export const postJoin = async (req, res) => {
   const { name, username, email, password, password2, location } = req.body;
   const pageTitle = "가입하기";
@@ -23,19 +25,53 @@ export const postJoin = async (req, res) => {
       errorMessage: "이미 사용중인 username/email 입니다.",
     });
   }
+  try {
+    await User.create({
+      name,
+      username,
+      email,
+      password,
+      password2,
+      location,
+    });
+    return res.redirect("/login");
+  } catch (error) {
+    return res.status(400).render("join", {
+      pageTitle,
+      errorMessage: error._message,
+    });
+  }
+};
 
-  await User.create({
-    name,
-    username,
-    email,
-    password,
-    password2,
-    location,
-  });
-  return res.redirect("/login");
+export const getLogin = (req, res) =>
+  res.render("login", { pageTitle: "로그인" });
+
+// 로그인 로직
+export const postLogin = async (req, res) => {
+  const { username, password } = req.body;
+  const pageTitle = "로그인";
+  const user = await User.findOne({ username });
+
+  // 계정 존재 유무 체크
+  if (!user) {
+    return res.status(400).render("login", {
+      pageTitle,
+      errorMessage: "존재하지 않는 아이디입니다.",
+    });
+  }
+
+  // 비밀번호 맞는지 체크
+  const ok = await bcrypt.compare(password, user.password);
+  if (!ok) {
+    return res.status(400).render("login", {
+      pageTitle,
+      errorMessage: "비밀번호가 일치하지 않습니다.",
+    });
+  }
+  console.log("유저 로그인 완료");
+  return res.redirect("/");
 };
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
-export const login = (req, res) => res.send("login");
 export const logout = (req, res) => res.send("logout");
 export const see = (req, res) => res.send("see user");
